de9ca9c339c6121da8152023b915cc2c9dedda57
ebhose <ebhose@localdomain.com>
2021-09-25 19:12:06 +0200

    Fix typo in file ssg/parsers.py

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -38,7 +38,7 @@ class ResourceParser(Parser):
     extensions = [".jpg", ".png", ".gif", ".css", ".html"]
 
     def parse(self, path: Path, source: Path, dest: Path):
-        copy(path, source, dest)
+        self.copy(path, source, dest)
 
 
 class MarkdownParser(Parser):
